8cf5ebe60c386f694af6218ec06ba087cabe34d7
Amanjain15 <amanjain.1596@gmail.com>
2018-01-28 01:44:43 +0530

    Map Modules Deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,9 +17,7 @@ import { UserComponent } from './user/user.component';
 import { TablesComponent } from './tables/tables.component';
 import { TypographyComponent } from './typography/typography.component';
 import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
+
 
 import { 
   AlertService,
@@ -36,10 +34,6 @@ import {
     TablesComponent,
     TypographyComponent,
     IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
-    UpgradeComponent
-
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -8,9 +8,6 @@ import { UserComponent } from './user/user.component';
 import { TablesComponent } from './tables/tables.component';
 import { TypographyComponent } from './typography/typography.component';
 import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
 
 const routes: Routes =[
     { path: 'dashboard',      component: HomeComponent },
@@ -18,9 +15,6 @@ const routes: Routes =[
     { path: 'table',          component: TablesComponent },
     { path: 'typography',     component: TypographyComponent },
     { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent },
-    { path: 'notifications',  component: NotificationsComponent },
-    { path: 'upgrade',        component: UpgradeComponent },
       { path: '',          redirectTo: 'dashboard', pathMatch: 'full' }
 ];
 
